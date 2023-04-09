adf0f1294ae8959569aedfea8ee316ea9622762b
rhaymisonbetini <rhaymisoncristian@gmail.com>
2023-04-09 09:47:20 -0300

    finish-scan-methods

--- portscan.sh
+++ portscan.sh
@@ -19,36 +19,74 @@ printf "${BBlue}################################################################
 #Email         	:
 ################################################################### \n\n"
 
-echo "SCOKET SCAN"
-echo "TYPE YOU SCAN CHOICE"
+echo "SELECT YOU SCAN CHOICE"
 echo "---------------------------------"
-echo "[1] - Scan avalible host"
-echo "[2] - Scan avalible port in hosts"
-echo "[3] - Scan avalible postn in host" 
+echo "[1] - Scan avalible host - default port 80"
+echo "[2] - Scan avalibles ports in hosts"
+echo "[3] - Scan avalible port in hosts" 
 
 read choice
 
 if [ $choice == "1" ]
 then
-	read -p "INSER TARGET EX:192.168.1 : " main
-	read -p "INSER INITAL: " init
-	read -p "INSET LIMIT: "  final
+	read -p "INSERT TARGET EX:192.168.1 : " main
+	read -p "INSERT INITAL: " init
+	read -p "INSERT LIMIT: "  final
 
 	if [ $init -ge 1 -a $final -le 254  -a main != "" ]
 		then
+			echo "SCAN INIT"
 			for host in `seq $init $final`
 				do
-					hping3 -S -p 80 -c 1 $main.$host 2> /dev/null | grep "flags=SA" 
+					hping3 -S -p 80 -c 1 $main.$host 2> /dev/null  
 				done
 					echo "SCAN FINISH!" 
 		else
 			echo "INVALID VALUES FOR INTERVAL"
-		fi
+	fi
 
 elif [ $choice == "2" ]
 then
-	echo"2"
+	read -p "INSERT THE TARGET EX: 192.157.1.30 : " host
+	read -p "INSERT INITAL PORT : " init
+	read -p "INSERT FINAL  PORT : " final
+
+	if [ $init -le -1 -o $init  -gt 65535 -o $init == "" ] || [ $final -le -1 -o $final -gt 65535 -o $final == "" ]
+		then
+		echo "PORT INTERVAL BETWEEN 0 AND 65535"
+		else
+		echo "INIT SCAN BY PORTS IN HOST: $host"
+			for port in `seq $init $final`
+				do
+					hping3 -S -p $port -c 1 $host 2> /dev/null
+				done
+					echo "SCAN FINISH!"
+	fi
 elif [ $choice == "3" ]
 then
-	echo "3"
+	read -p "INSERT THE TARGET EX: 192.157.1 : " main
+	read -p "INSERT INITAL : " init
+	read -p "INSERT FINAL : " final
+	read -p "INSERT PORT TO SCAN (0->65535) : "  port
+
+	if [ $main == "" -o $init == "" -o $final == "" -o $port == "" ] || [ $port -le -1 -o $port -gt 65535 ]
+		then 
+		echo "INVALID PARAMS"
+		else
+		echo "INIT SCAN PORT: $port IN HOSTS!"
+			for host in `seq $init $final`
+				do
+					hping3 -S -p $port -c 1 $main.$host 2> /dev/null
+				done
+					echo "SCAN FINISH!"
+	fi
 fi
+
+
+
+
+
+
+
+
+
